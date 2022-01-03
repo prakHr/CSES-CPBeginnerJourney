cc9be02f038162547048de0879c523481a1845eb
prakHr <gprakhar0@gmail.com>
2022-01-03 15:57:04 +0530

    Add files via upload

---
+++ Geometry/Line Segment Intersection.cpp
@@ -0,0 +1,171 @@
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define int long long 
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define PI acos(-1)
+#define sqr(a) ((a)*(a))
+#define ff first
+#define ss second
+#define sf scanf
+#define pf printf
+#define pf1(a) pf("%lld", a);
+#define pf2(a,b) pf("%lld %lld", a, b)
+#define pf3(a,b,c) pf("%lld %lld %lld", a, b, c)
+#define sf1(a) sf("%lld", &a)
+#define sf2(a,b) sf("%lld %lld", &a, &b)
+#define sf3(a,b,c) sf("%lld %lld %lld", &a, &b, &c)
+#define sf4(a,b,c,d) sf("%lld %lld %lld %lld", &a, &b, &c, &d)
+#define sf5(a,b,c,d,e) sf("%lld %lld %lld %lld %lld", &a, &b, &c, &d, &e)
+#define sfc(a) sf(" %c", &a)
+#define sfd(n) sf("%lf", &n)
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define RESET(a, b) memset(a, (b), sizeof(a))
+#define lb lower_bound
+#define ub upper_bound
+#define nl pf("\n")
+#define endl '\n'
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define SZ(v) (int)v.size()
+#define ALL(v) v.begin(), v.end()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define UNIQUE(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define on(n,i) (n|=(1LL<<i))
+#define isOn(n,i) (n&(1LL<<i))
+#define off(n,i) (n = isOn(n,i) ? n ^ (1LL<<i) : n)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a/gcd(a,b)*b)
+#define watch(a) cout << (#a) << " is " << (a) << '\n'
+#define watch2(a,b) cout << (#a) << " is " << (a) << " and " << (#b) << " is " << (b) << '\n'
+#define MIN3(a,b,c) MIN(a, MIN(b,c))
+#define MAX3(a,b,c) MAX(a, MAX(b,c))
+ 
+using namespace std;
+typedef long long ll; 
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef map<int,int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+typedef tuple<int,int,int> State;
+ 
+template <class T>  inline void chmax(T &x,T &y) {if(x < y) swap(x,y);}
+template <class T>  inline void chmin(T &x,T &y) {if(x > y) swap(x,y);}
+ 
+/*----------------------Graph Moves----------------*/
+//const int fx[]={+1,-1,+0,+0};
+//const int fy[]={+0,+0,+1,-1};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+const int MOD = 1e9 + 7;
+const double EPS = 10e-10;
+const int maxN = 40;
+
+struct Point
+{
+	int x;
+	int y;
+};
+
+// Given three collinear points p, q, r, the function checks if
+// point q lies on line segment 'pr'
+bool onSegment(Point p, Point q, Point r)
+{
+	if (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) &&
+		q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y))
+	return true;
+
+	return false;
+}
+
+// To find orientation of ordered triplet (p, q, r).
+// The function returns following values
+// 0 --> p, q and r are collinear
+// 1 --> Clockwise
+// 2 --> Counterclockwise
+int orientation(Point p, Point q, Point r)
+{
+	// See https://www.geeksforgeeks.org/orientation-3-ordered-points/
+	// for details of below formula.
+	int val = (q.y - p.y) * (r.x - q.x) -
+			(q.x - p.x) * (r.y - q.y);
+
+	if (val == 0) return 0; // collinear
+
+	return (val > 0)? 1: 2; // clock or counterclock wise
+}
+
+// The main function that returns true if line segment 'p1q1'
+// and 'p2q2' intersect.
+bool doIntersect(Point p1, Point q1, Point p2, Point q2)
+{
+	// Find the four orientations needed for general and
+	// special cases
+	int o1 = orientation(p1, q1, p2);
+	int o2 = orientation(p1, q1, q2);
+	int o3 = orientation(p2, q2, p1);
+	int o4 = orientation(p2, q2, q1);
+
+	// General case
+	if (o1 != o2 && o3 != o4)
+		return true;
+
+	// Special Cases
+	// p1, q1 and p2 are collinear and p2 lies on segment p1q1
+	if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+
+	// p1, q1 and q2 are collinear and q2 lies on segment p1q1
+	if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+
+	// p2, q2 and p1 are collinear and p1 lies on segment p2q2
+	if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+
+	// p2, q2 and q1 are collinear and q1 lies on segment p2q2
+	if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+
+	return false; // Doesn't fall in any of the above cases
+}
+
+// Driver program to test above functions
+int32_t main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int x1,y1,x2,y2,x3,y3,x4,y4;
+		cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4;
+		struct Point p1={x1,y1},q1={x2,y2};
+		struct Point p2={x3,y3},q2={x4,y4};
+		doIntersect(p1, q1, p2, q2)? cout << "YES\n": cout << "NO\n";
+	}
+//	struct Point p1 = {1, 1}, q1 = {10, 1};
+//	struct Point p2 = {1, 2}, q2 = {10, 2};
+//
+//	doIntersect(p1, q1, p2, q2)? cout << "Yes\n": cout << "No\n";
+//
+//	p1 = {10, 0}, q1 = {0, 10};
+//	p2 = {0, 0}, q2 = {10, 10};
+//	doIntersect(p1, q1, p2, q2)? cout << "Yes\n": cout << "No\n";
+//
+//	p1 = {-5, -5}, q1 = {0, 0};
+//	p2 = {1, 1}, q2 = {10, 10};
+//	doIntersect(p1, q1, p2, q2)? cout << "Yes\n": cout << "No\n";
+
+	return 0;
+}

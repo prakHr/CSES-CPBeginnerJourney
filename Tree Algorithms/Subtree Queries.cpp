ea528f04236e51bf862e508f30c95f5dfa2c6850
prakHr <gprakhar0@gmail.com>
2022-01-03 16:01:35 +0530

    Add files via upload

---
+++ Tree Algorithms/Subtree Queries.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define int long long
+#define double long double
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define PI acos(-1)
+#define sqr(a) ((a)*(a))
+#define ff first
+#define ss second
+#define sf scanf
+#define pf printf
+#define pf1(a) pf("%lld", a);
+#define pf2(a,b) pf("%lld %lld", a, b)
+#define pf3(a,b,c) pf("%lld %lld %lld", a, b, c)
+#define sf1(a) sf("%lld", &a)
+#define sf2(a,b) sf("%lld %lld", &a, &b)
+#define sf3(a,b,c) sf("%lld %lld %lld", &a, &b, &c)
+#define sf4(a,b,c,d) sf("%lld %lld %lld %lld", &a, &b, &c, &d)
+#define sf5(a,b,c,d,e) sf("%lld %lld %lld %lld %lld", &a, &b, &c, &d, &e)
+#define sfc(a) sf(" %c", &a)
+#define sfd(n) sf("%lf", &n)
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define RESET(a, b) memset(a, (b), sizeof(a))
+#define lb lower_bound
+#define ub upper_bound
+#define nl pf("\n")
+#define endl '\n'
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define SZ(v) (int)v.size()
+#define ALL(v) v.begin(), v.end()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define UNIQUE(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define on(n,i) (n|=(1LL<<i))
+#define isOn(n,i) (n&(1LL<<i))
+#define off(n,i) (n = isOn(n,i) ? n ^ (1LL<<i) : n)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a/gcd(a,b)*b)
+#define watch(a) cout << (#a) << " is " << (a) << '\n'
+#define watch2(a,b) cout << (#a) << " is " << (a) << " and " << (#b) << " is " << (b) << '\n'
+#define MIN3(a,b,c) MIN(a, MIN(b,c))
+#define MAX3(a,b,c) MAX(a, MAX(b,c))
+ 
+using namespace std;
+typedef long long ll; 
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef map<int,int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+typedef tuple<int,int,int> State;
+ 
+template <class T>  inline void chmax(T &x,T &y) {if(x < y) swap(x,y);}
+template <class T>  inline void chmin(T &x,T &y) {if(x > y) swap(x,y);}
+ 
+/*----------------------Graph Moves----------------*/
+//const int fx[]={+1,-1,+0,+0};
+//const int fy[]={+0,+0,+1,-1};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+ 
+const int INF = 0x3f3f3f3f3f3f;
+const int MOD = 1e9 + 7;
+const double EPS = 10e-10;
+const int maxN = 2e5+6;
+int N,Q,timer;
+int in[maxN],out[maxN];
+vector<int> adj[maxN];
+int arr[maxN],fenwickTree[2*maxN];
+
+void update(int idx, ll val){
+    for(int i = idx; i < 2*maxN; i += i&-i)
+        fenwickTree[i] += val;
+}
+
+ll query(int idx){
+    ll sum = 0;
+    for(int i = idx; i > 0; i -= i&-i)
+        sum += fenwickTree[i];
+    return sum;
+}
+
+void dfs(int node,int parent=0){
+	in[node]=++timer;
+	for(int child:adj[node]){
+		if(child!=parent){
+			dfs(child,node);
+		
+		}
+	}
+	out[node]=++timer;
+}
+int32_t main() {
+   	cin>>N>>Q;
+   	FOR(i,1,N+1,1){
+   		cin>>arr[i];	
+	}
+   	REP(i,N-1){
+   		int u,v;
+		cin>>u>>v;
+	    adj[u].pb(v);
+	    adj[v].pb(u);	
+	
+	}
+	timer = 0;
+	dfs(1);
+    FOR(i,1,N+1,1){
+   		update(out[i],arr[i]);	
+	}
+	while(Q--){
+		int type,idx;
+		cin>>type>>idx;
+		if(type==1){
+			update(out[idx],-arr[idx]);
+			int val;
+			cin>>val;
+			arr[idx]=val;
+			update(out[idx],arr[idx]);
+		}
+		else if(type==2){
+			cout<<query(out[idx])-query(in[idx])<<endl;
+		}
+	}
+	return 0;
+}
---
+++ Tree Algorithms/Tree Distances I.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define int long long
+#define double long double
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define PI acos(-1)
+#define sqr(a) ((a)*(a))
+#define ff first
+#define ss second
+#define sf scanf
+#define pf printf
+#define pf1(a) pf("%lld", a);
+#define pf2(a,b) pf("%lld %lld", a, b)
+#define pf3(a,b,c) pf("%lld %lld %lld", a, b, c)
+#define sf1(a) sf("%lld", &a)
+#define sf2(a,b) sf("%lld %lld", &a, &b)
+#define sf3(a,b,c) sf("%lld %lld %lld", &a, &b, &c)
+#define sf4(a,b,c,d) sf("%lld %lld %lld %lld", &a, &b, &c, &d)
+#define sf5(a,b,c,d,e) sf("%lld %lld %lld %lld %lld", &a, &b, &c, &d, &e)
+#define sfc(a) sf(" %c", &a)
+#define sfd(n) sf("%lf", &n)
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define RESET(a, b) memset(a, (b), sizeof(a))
+#define lb lower_bound
+#define ub upper_bound
+#define nl pf("\n")
+#define endl '\n'
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define SZ(v) (int)v.size()
+#define ALL(v) v.begin(), v.end()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define UNIQUE(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define on(n,i) (n|=(1LL<<i))
+#define isOn(n,i) (n&(1LL<<i))
+#define off(n,i) (n = isOn(n,i) ? n ^ (1LL<<i) : n)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a/gcd(a,b)*b)
+#define watch(a) cout << (#a) << " is " << (a) << '\n'
+#define watch2(a,b) cout << (#a) << " is " << (a) << " and " << (#b) << " is " << (b) << '\n'
+#define MIN3(a,b,c) MIN(a, MIN(b,c))
+#define MAX3(a,b,c) MAX(a, MAX(b,c))
+ 
+using namespace std;
+typedef long long ll; 
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef map<int,int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+typedef tuple<int,int,int> State;
+ 
+template <class T>  inline void chmax(T &x,T &y) {if(x < y) swap(x,y);}
+template <class T>  inline void chmin(T &x,T &y) {if(x > y) swap(x,y);}
+ 
+/*----------------------Graph Moves----------------*/
+//const int fx[]={+1,-1,+0,+0};
+//const int fy[]={+0,+0,+1,-1};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+ 
+const int INF = 0x3f3f3f3f3f3f;
+const int MOD = 1e9 + 7;
+const double EPS = 10e-10;
+const int maxN = 2e5+1;
+int dist[2][maxN];
+vi adj[maxN];
+
+int dfs(int u,int p,int d,int i){
+	dist[i][u]=d;
+	int opt=-1;
+	for(int v:adj[u]){
+		if(v!=p){
+			int x = dfs(v,u,d+1,i);
+			if(opt==-1 or dist[i][x]>dist[i][opt]){
+				opt=x;
+			}
+		}
+	}
+	return opt == -1 ? u : opt;
+}
+ 
+int32_t main() {
+   	
+    int n;
+	cin>>n;
+	for(int i=1;i<n;i++){
+    	int a,b;
+    	cin>>a>>b;
+    	a--;b--;
+    	adj[a].pb(b);
+    	adj[b].pb(a);
+	}
+	// first, find node a by finding the farthest node from an arbitrary node x
+	int A = dfs(0,0,0,0);
+	// then, find node b (this step also computes distance from a to every other node)
+	int B = dfs(A,A,0,0);
+	// finally, compute the distance from b to every other node
+	dfs(B,B,0,1);
+	REP(i,n){
+		cout<<max(dist[0][i],dist[1][i])<<" \n"[i==n-1];
+	}
+	
+	return 0;
+}
